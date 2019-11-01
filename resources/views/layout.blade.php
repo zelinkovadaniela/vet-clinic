7899c20abc82ffe0e42b65e5e66a99fb5b900f2e
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2019-11-01 10:23:48 +0100

    add react

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,15 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    
     <title>St. Hector's Veterinary Clinic</title>
 </head>
 <body>
 
-    <div id="app"></div>
-
-    <script src="{{ mix('js/app.js') }}"></script>
+    @yield('content')
     
 </body>
 </html>
