b556209f647a47ab7789279317bf345dc42cf444
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2019-11-01 10:23:48 +0100

    add react

---
+++ resources/js/App/Components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+        return (
+            <main>
+                <h1>St. Hector's Veterinary Clinic</h1>
+                
+            </main>
+        )
+    }
+}
